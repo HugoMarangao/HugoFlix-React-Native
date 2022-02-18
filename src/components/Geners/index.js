f1f52953e9936883995bef18efa601c73c0c82c8
Hugo Marangão Souza <79268315+HugoMarangao@users.noreply.github.com>
2022-02-18 20:00:01 -0300

    src

--- src/components/Geners/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Container,Name } from "./styles";
-export default function Genres({data}){
-    return(
-        <Container>
-            <Name>{data.name}</Name>
-        </Container>
-    )
-}
